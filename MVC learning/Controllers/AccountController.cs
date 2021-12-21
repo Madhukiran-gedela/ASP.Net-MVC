401e69fee94251cf701b69c5288676e3697bd601
madhu kiran <madhukiran.gedela@codebees.com>
2021-12-21 17:22:50 +0530

    intial Commit

---
+++ MVC learning/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_learning.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string Username,string Password)
+        {
+            if (Username == "admin" && Password == "man")
+                return RedirectToAction("Dashboard", "admin");
+            else
+                return RedirectToAction("Invalid Login");
+        }
+        public ActionResult IvalidLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC learning/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_learning.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View("OurcompanyProducts");
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Studentdetails()
+        {
+            ViewBag.StudentId = 221;
+            ViewBag.StudentName = "Madhu";
+            ViewBag.StudentMarks = 100;
+            return View();
+        }
+        public ActionResult GetEmpName(int EmpId)
+        {
+            var Employees = new[]
+            {
+                new {EmpId = 101,EmpName="Madhu",Saraly=10000},
+                new {EmpId = 102,EmpName="Kiran",Saraly=30000},
+                new {EmpId = 103,EmpName="Raju",Saraly=30000},
+            };
+            string matchEmpName = null;
+            foreach (var item in Employees)
+            {
+                if (item.EmpId == EmpId)
+                {
+                    matchEmpName = item.EmpName;
+                }
+            }
+            return Content(matchEmpName, "text/plain");
+        }
+        public ActionResult EmpFb(int EmpId)
+        {
+            var Employees = new[]
+            {
+                new {EmpId = 101,EmpName="Madhu",Saraly=10000},
+                new {EmpId = 102,EmpName="Kiran",Saraly=30000},
+                new {EmpId = 103,EmpName="Raju",Saraly=30000},
+            };
+            string fburl = null;
+            foreach (var item in Employees)
+            {
+                if (item.EmpId == EmpId)
+                {
+
+                    fburl = "https://www.google.com/webhp?authuser=1" + EmpId;
+                }
+            }
+            if (fburl == null)
+            {
+                return Content("invalid emp id ");
+            }
+            else
+            {
+                return Redirect(fburl);
+            }
+        }
+    }
+}
